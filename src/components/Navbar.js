302cbdb2c143ff6f46b7277b25505e33e030f982
miguelcamargo90 <miguel.lemecamargo@gmail.com>
2023-12-03 18:12:21 -0300

    Novo commit11

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import logo from "../images/logo.png";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../images/logo.png';
 import TypingEffect from './TypingEffect';
 import { FaWhatsapp, FaImages } from 'react-icons/fa';
 
 export default function Navbar() {
   const { hash } = useLocation();
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     if (hash === '' || hash === '#') return;
@@ -20,10 +21,15 @@ export default function Navbar() {
   return (
     <header className="bg-white md:sticky top-0 z-10 shadow-md">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <button onClick={() => setMenuOpen(!isMenuOpen)} className="md:hidden">
+        <span className="bg-gray-900 w-6 h-1 block mb-1"></span>
+        <span className="bg-gray-900 w-6 h-1 block mb-1"></span>
+        <span className="bg-gray-900 w-6 h-1 block"></span>
+      </button>
         <Link to="/">
           <img src={logo} alt="Logo" className="w-40 h-50" />
         </Link>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center md:flex">
           <Link to="/#about" className={buttonStyle}>Sobre Nós</Link>
           <Link to="/#projects" className={buttonStyle}>Serviços & Soluções</Link>
           <Link to="/#products" className={buttonStyle}>Produtos</Link>
